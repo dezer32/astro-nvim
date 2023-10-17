66e08422b4d551022ba1b8886073d87aed2ff95e
Dezer <my@ipro.dev>
2023-10-17 07:49:24 +0300

    cfg

--- init.lua
+++ init.lua
@@ -26,44 +26,44 @@ return {
   --   underline = true,
   -- },
   --
-  -- lsp = {
-  --   -- customize lsp formatting options
-  --   formatting = {
-  --     -- control auto formatting on save
-  --     format_on_save = {
-  --       enabled = true, -- enable or disable format on save globally
-  --       allow_filetypes = { -- enable format on save for specified filetypes only
-  --         -- "go",
-  --       },
-  --       ignore_filetypes = { -- disable format on save for specified filetypes
-  --         -- "python",
-  --       },
-  --     },
-  --     disabled = { -- disable formatting capabilities for the listed language servers
-  --       -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-  --       -- "lua_ls",
-  --     },
-  --     timeout_ms = 1000, -- default format timeout
-  --     -- filter = function(client) -- fully override the default formatting function
-  --     --   return true
-  --     -- end
-  --   },
-  --   -- enable servers that you already have installed without mason
-  --   servers = {
-  --     -- "pyright"
-  --   },
-  -- },
-  --
-  -- Configure require("lazy").setup() options
-  -- lazy = {
-  --   defaults = { lazy = true },
-  --   performance = {
-  --     rtp = {
-  --       -- customize default disabled vim plugins
-  --       disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin" },
-  --     },
-  --   },
-  -- },
+  lsp = {
+    --   -- customize lsp formatting options
+    formatting = {
+      --     -- control auto formatting on save
+      format_on_save = {
+        enabled = true, -- enable or disable format on save globally
+        --       allow_filetypes = { -- enable format on save for specified filetypes only
+        --         -- "go",
+        --       },
+        --       ignore_filetypes = { -- disable format on save for specified filetypes
+        --         -- "python",
+        --       },
+      },
+      --     disabled = { -- disable formatting capabilities for the listed language servers
+      --       -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
+      --       -- "lua_ls",
+      --     },
+      --     timeout_ms = 1000, -- default format timeout
+      --     -- filter = function(client) -- fully override the default formatting function
+      --     --   return true
+      --     -- end
+      --   },
+      --   -- enable servers that you already have installed without mason
+      --   servers = {
+      --     -- "pyright"
+      -- },
+    },
+    --
+    -- Configure require("lazy").setup() options
+    -- lazy = {
+    --   defaults = { lazy = true },
+    --   performance = {
+    --     rtp = {
+    --       -- customize default disabled vim plugins
+    --       disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin" },
+    --     },
+    --   },
+  },
   --
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
